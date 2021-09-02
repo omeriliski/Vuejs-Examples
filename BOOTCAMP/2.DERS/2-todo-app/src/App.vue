2d7aa8cd72df8f9186175cb3b766e17761b31a8e
Ömer Iliski <omeriliski06@gmail.com>
2021-09-02 23:54:24 +0200

    added todo app

--- BOOTCAMP/2.DERS/2-todo-app/src/App.vue
+++ BOOTCAMP/2.DERS/2-todo-app/src/App.vue
@@ -1,8 +1,48 @@
 <template>
   <div class="container">
     <h3 class="text-center">Todo App</h3>
-    <hr class="my-2">
-      <label for="todoText"></label>
-      <input type="text" id="todoText" placeholder="Add todo...">
+    <hr class="my-2" />
+    <label for="todoText"></label>
+    <input
+      type="text"
+      id="todoText"
+      placeholder="Add todo..."
+      v-model="todoText"
+      @keydown.enter="addTodo"
+    />
+    <ul>
+      <li
+        v-for="todo in todoList"
+        :key="todo.id"
+        class="d-flex justify-content-between align-items-center"
+      >
+        <span>{{ todo }}</span>
+        <button class="sm red" @click="deleteTodo(todo)">Sil</button>
+      </li>
+    </ul>
+    <small class="d-flex justify-content-end green mt-2">todo sayisi: {{todoList.length}}</small>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoText: "",
+      todoList: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push(this.todoText);
+      this.todoText = "";
+    },
+    deleteTodo(todo){
+      this.todoList = this.todoList.filter(t=> t!=todo)
+      // let index = this.todoList.indexOf(todo);
+      // let removed = this.todoList.splice(index,1);
+      // console.log(removed)
+    }
+  },
+};
+</script>
