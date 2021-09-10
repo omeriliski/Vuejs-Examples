c53519e573c8fab9c2e8e98329ae880cbbae221b
Ömer Iliski <omeriliski06@gmail.com>
2021-09-11 00:28:38 +0200

    added vuex getter

--- BOOTCAMP/4.LESSON/vuex-intro/src/main.js
+++ BOOTCAMP/4.LESSON/vuex-intro/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store';
+import '@/assets/style.css'
 
 const app = createApp(App)
 app.use(store)
--- BOOTCAMP/4.LESSON/vuex-intro/src/store.js
+++ BOOTCAMP/4.LESSON/vuex-intro/src/store.js
@@ -2,13 +2,32 @@ import {createStore} from "vuex";
 
 const store=createStore({
     state:{
-        person:{
+        user:{
             name:"Ömer",
             lastName:"iliski",
-            age:39
+            age:39,
+            password:"1564sddöl8645sfkölf"
         },
         theme:"dark",
-        permissions:[1,2,3,4,5]
+        permissions:[1,2,3,4,5],
+        userList:["Ömer","Osman","Selman","Mehmet","Burak"],
+        itemList:[
+            {id:1, title:"masa", type:"mobilya"},
+            {id:2, title:"sandalye", type:"mobilya"},
+            {id:3, title:"TV", type:"elektronik"},
+            {id:4, title:"monitör", type:"elektronik"},
+            {id:5, title:"bardak", type:"plastik"},
+        ]
+    },
+    getters:{
+        woodItems:state=>state.itemList.filter(i=>i.type=="mobilya"),
+        activeUser(state){
+            const user={
+                ...state.user
+            }
+            delete user.password
+            return user
+        }
     }
 })
 
