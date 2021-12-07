c3fcdc1ff52dfd9c154c0e2071ce8d2f2160e038
Ömer Iliski <omeriliski06@gmail.com>
2021-12-07 23:22:57 +0100

    finished todoList

---
+++ BOOTCAMP/9.LESSON/todo-list/src/components/addSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="w-full">
+    <input
+        @keydown.enter="newItem"
+      type="text"
+      placeholder="write your todo"
+      class="
+        w-full
+        px-2
+        py-1
+        bg-gray-700
+        border-[1px] border-gray-400
+        rounded-md
+        outline-none
+      "
+    />
+  </section>
+</template>
+
+<script>
+export default {
+    props:{
+        addTodo:Function
+    },
+    setup(props) {
+      const newItem=(event)=>{
+          props.addTodo(event.target.value)
+          event.target.value=""
+      }  
+      return {newItem }
+    }
+}
+</script>
---
+++ BOOTCAMP/9.LESSON/todo-list/src/components/summary.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="text-green-300">{{completed}} is done</div>
+        <div class="text-red-400">{{uncompleted}} is not done</div>
+    </div>
+</template>
+
+<script>
+import {computed} from 'vue'
+export default {
+    props:{
+        todoList:Array
+    },
+    setup(props) {
+        const completed=computed(()=>props.todoList.filter(e =>e.completed).length);
+        const uncompleted=computed(()=>props.todoList.filter(e =>!e.completed).length)
+        return {completed,uncompleted}
+    }
+}
+</script>
