e5a09b270cdb928d7da58aaf51ba703d3930b271
Ömer Iliski <omeriliski06@gmail.com>
2021-12-13 14:36:23 +0100

    added review1

---
+++ BOOTCAMP/11.LESSON/suspense-example/src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="todo in todoList" :key="todo.id">{{todo.title}}</li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue';
+const todoList=ref([]);
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => todoList.value=json)
+  
+</script>
