b929f774baf6a641b457d5d2cd750e609f1c5c98
Ömer Iliski <omeriliski06@gmail.com>
2021-12-14 21:34:16 +0100

    added composition api review

--- BOOTCAMP/9.10.LESSON REVIEW/review1/src/App.vue
+++ BOOTCAMP/9.10.LESSON REVIEW/review1/src/App.vue
@@ -1,21 +1,31 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <input type="text" v-model="text" />
+    <h3>{{ text }}</h3>
+    <p v-if="isWriting">writing...</p>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import { ref, watch } from "vue";
+export default {
+  setup() {
+    const text = ref("");
+    const writing = ref("");
+    let isWriting = ref(false);
+    watch(text, () => {
+      if (text.value.length > 0) 
+      {
+        console.log("calisti1")
+        isWriting.value = true;
+      }
+      setTimeout(() => {
+        isWriting.value=false
+        console.log("calisti2");
+      }, 1500);
+    });
+
+    return { text, writing,isWriting };
+  },
+};
+</script>
