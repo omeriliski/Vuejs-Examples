769803fa7ba063f7137a3d99381ba33d4153a345
Ömer Iliski <omeriliski06@gmail.com>
2021-12-16 16:06:12 +0100

    added namespaced-components

---
+++ BOOTCAMP/11.LESSON/namespaced-components/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Form.Label>
+      Header
+      <Form.Input/>
+    </Form.Label>
+
+    <Form.Label>
+      Kategori
+      <Form.Select/>
+    </Form.Label>
+  </div>
+</template>
+
+<script setup>
+import * as Form from "./components/form-components";
+</script>
